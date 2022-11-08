cedfc8c0641b880bc1a20957d4045efac285b430
Sergey <domarch388640@gmail.com>
2022-11-08 15:53:11 +0200

    complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import '../src/css/common.css';
 
 // validate password -- practice
 
 // import validatePasword from './validate-password';
 // console.log(validatePasword('qwerty9876Gty'));
+console.log('qwerty');
+console.log('hi, people');
