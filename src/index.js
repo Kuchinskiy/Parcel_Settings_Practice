a974abeafb0cb39c717069b1b6b7e483388304b7
Sergey <domarch388640@gmail.com>
2022-11-08 21:11:28 +0200

    complete

--- src/index.js
+++
@@ -1,6 +0,0 @@
-
-// validate password -- practice
-
-// import validatePasword from './validate-password';
-// console.log(validatePasword('qwerty9876Gty'));
-
--- src/js/colorpicker.js
+++
@@ -1,3 +0,0 @@
-
-
-// import colors from '../js/colors.json';
